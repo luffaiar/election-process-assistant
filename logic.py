2f64923f24220cdbee4c6da65f951540d0e7ad60
luffaiar <luffaiarmohammath@gmail.com>
2026-05-02 08:23:34 +0530

    Delete logic.py

--- logic.py
+++
@@ -1,7 +0,0 @@
-from nlp import get_nlp_response
-
-def get_response(user_input):
-    try:
-        return get_nlp_response(user_input)
-    except:
-        return "Sorry, I couldn't understand. Try asking about voting or registration."
